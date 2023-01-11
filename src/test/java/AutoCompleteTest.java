341c8df2df8459a0a324769ab4757181483ca25a
Balint Meszaros <meszaros.balint65@gmail.com>
2023-01-11 15:00:47 +0100

    Add: Test annotations

--- src/test/java/AutoCompleteTest.java
+++ src/test/java/AutoCompleteTest.java
@@ -1,6 +1,7 @@
 import autocomplete.Autocomplete;
 import datastructure.Trie;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class AutoCompleteTest {
@@ -14,18 +15,21 @@ public class AutoCompleteTest {
         this.autocomplete = new Autocomplete(trie);
     }
 
-
+    @Test
     public void testNewAutoCompleteInstanceCreate(){
         assertTrue(this.autocomplete instanceof Autocomplete);
     }
+    @Test
     public void testAfterOneInsert_GetMatcherCanReturnTheInsertedWord(){
         this.autocomplete.trie.insert("autocomplete");
         assertFalse(this.autocomplete.getMatches("autocomplete").isEmpty());
     }
+    @Test
     public void testAfterInsertAuto_GetMatchesCantFindAutocomplete(){
         this.autocomplete.trie.insert("auto");
         assertTrue(this.autocomplete.getMatches("autocomplete").isEmpty());
     }
+    @Test
     public void testAfterInsertCaseSensitiveAutocomplete_GetMatchesCantFindIt_ByCaseInsensitiveAuto(){
         this.autocomplete.trie.insert("Autocomplete");
         assertTrue(this.autocomplete.getMatches("auto").isEmpty());
