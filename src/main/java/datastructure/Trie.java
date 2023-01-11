f62b6f81db084bd40d5b318e3a3ac665bbc6a1c7
Balint Meszaros <meszaros.balint65@gmail.com>
2023-01-11 15:01:14 +0100

    Add: word inserting

--- src/main/java/datastructure/Trie.java
+++ src/main/java/datastructure/Trie.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.Optional;
+
 public class Trie {
 
     public TrieNode root;
@@ -9,7 +11,39 @@ public class Trie {
     }
 
     public void insert(String word) {
-        // TODO
+        if (word.length() == 0) {
+            throw new IllegalArgumentException("Inserted word must be at least 1 character long!");
+        } else if (word.length() == 1) {
+            char character = word.charAt(0);
+            setTerminatingCharacterInRoot(character);
+        } else {
+            char firstCharacter = word.charAt(0);
+            String restOfString = word.substring(1);
+            TrieNode newNode = addFirstCharacterInRoot(firstCharacter);
+            newNode.insert(restOfString);
+        }
+    }
+
+    private void setTerminatingCharacterInRoot(char character) {
+        Optional<TrieNode> child = root.getChildByCharacter(character);
+        if (child.isPresent()) {
+            child.get().setTerminating(true);
+        } else {
+            TrieNode newNode = new TrieNode(character);
+            newNode.setTerminating(true);
+            root.setChild(newNode);
+        }
+    }
+
+    private TrieNode addFirstCharacterInRoot(char character) {
+        Optional<TrieNode> child = root.getChildByCharacter(character);
+        if (child.isPresent()) {
+            return child.get();
+        } else {
+            TrieNode newNode = new TrieNode(character);
+            root.setChild(newNode);
+            return newNode;
+        }
     }
 
     public boolean remove(String word) {
--- src/main/java/datastructure/TrieNode.java
+++ src/main/java/datastructure/TrieNode.java
@@ -2,6 +2,8 @@ package datastructure;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class TrieNode {
 
@@ -14,4 +16,46 @@ public class TrieNode {
         this.isTerminating = false;
         this.children = new ArrayList<>();
     }
+
+    public void setChild(TrieNode trieNode) {
+       this.children.add(trieNode);
+    }
+
+    public Optional<TrieNode> getChildByCharacter(char value) {
+        return this.children.stream().filter(node -> node.value == value).findFirst();
+    }
+
+    public void setTerminating(boolean terminating) {
+        isTerminating = terminating;
+    }
+
+    public void insert(String restOfString) {
+        if (restOfString.length() == 1) {
+            char character = restOfString.charAt(0);
+            TrieNode newNode = new TrieNode(character);
+            newNode.setTerminating(true);
+            setChild(newNode);
+        } else {
+            char firstCharacter = restOfString.charAt(0);
+            String restOfRestOfString = restOfString.substring(1);
+            TrieNode newNode = new TrieNode(firstCharacter);
+            setChild(newNode);
+            newNode.insert(restOfRestOfString);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrieNode trieNode = (TrieNode) o;
+        return value == trieNode.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, isTerminating, children);
+    }
+
+
 }
